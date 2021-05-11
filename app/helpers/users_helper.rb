3d90f1381c780c45c9836b1e908c885d32f57cef
karuizawakingdom <ubuntu@ip-172-31-13-97.us-east-2.compute.internal>
2021-05-11 10:17:30 +0000

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravator_for(user, options = {size: 80})
+    size = options[:size]
+    gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}?s=#{size}"
+    image_tag(gravator_url, alt: user.name, class: "gravatar")
+  end
 end
