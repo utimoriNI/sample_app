ca2302fd7cdbf32fb3c99da638c0b847788d3b48
karuizawakingdom <ubuntu@ip-172-31-13-97.us-east-2.compute.internal>
2021-05-19 02:55:53 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 保存に成功したときの処理
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
