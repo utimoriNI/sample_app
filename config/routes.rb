e755e79bda981ff872d63f81a3e36692527c2cdc
karuizawakingdom <ubuntu@ip-172-31-13-97.us-east-2.compute.internal>
2021-06-05 08:29:10 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   get 'users/new'
   get '/help',      to: 'static_pages#help'
@@ -10,5 +12,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
