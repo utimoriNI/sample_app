02e4971b7ea7806721dbe9a5faf95b8ad78df4f7
karuizawakingdom <ubuntu@ip-172-31-13-97.us-east-2.compute.internal>
2021-06-05 12:43:32 +0000

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
-    :addresses      => ENV['MAILGUN_SMTP_SERVER'],
+    :address      => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => host,
